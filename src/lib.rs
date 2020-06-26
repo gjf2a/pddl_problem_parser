5952c36fbb1851648a3275436efbd4ce4ed4106a
gjf2a <ferrer@hendrix.edu>
2020-06-26 08:46:17 -0500

    Removed all panics and assertions and replaced them with proper io errors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::collections::{BTreeSet, BTreeMap};
+use std::io;
+use std::io::{Error, ErrorKind};
 
 struct Tokenizer {
     pending: String,
@@ -96,46 +98,60 @@ pub struct PddlParser {
     problem: PddlProblem
 }
 
+pub fn errorize<T>(msg: String) -> io::Result<T> {
+    Err(Error::new(ErrorKind::Other, msg.as_str()))
+}
+
 impl PddlParser {
-    pub fn parse(pddl: &str) -> PddlProblem {
+    pub fn parse(pddl: &str) -> io::Result<PddlProblem> {
         let mut parser = PddlParser {
             tokens: Tokenizer::new("()").tokenize(pddl),
             i: 0, problem: PddlProblem::new() };
-        parser.define();
-        parser.problem
+        parser.define()?;
+        Ok(parser.problem)
     }
 
-    fn at_close(&self) -> bool {
-        self.token() == ")"
+    fn at_close(&self) -> io::Result<bool> {
+        Ok(self.token()? == ")")
     }
 
-    fn snag_predicate(&mut self) -> Predicate {
-        self.check("(");
-        let mut result = Predicate::new(self.snag());
-        while !self.at_close() {
-            result.add_arg(self.snag());
+    fn snag_predicate(&mut self) -> io::Result<Predicate> {
+        self.check("(")?;
+        let mut result = Predicate::new(self.snag()?);
+        while !self.at_close()? {
+            result.add_arg(self.snag()?);
         }
-        self.check(")");
-        result
+        self.check(")")?;
+        Ok(result)
     }
 
-    fn snag(&mut self) -> String {
-        let result = String::from(self.token());
+    fn snag(&mut self) -> io::Result<String> {
+        let token = self.token()?;
+        let result = String::from(token);
         self.advance();
-        result
+        Ok(result)
     }
 
-    fn token(&self) -> &str {
+    fn token(&self) -> io::Result<&str> {
         self.lookahead(0)
     }
 
-    fn lookahead(&self, distance: usize) -> &str {
-        self.tokens[self.i + distance].as_str()
+    fn lookahead(&self, distance: usize) -> io::Result<&str> {
+        let index = self.i + distance;
+        match self.tokens.get(index) {
+            Some(s) => Ok(s.as_str()),
+            None => errorize(format!("Token index '{}'; {} tokens available", index, self.tokens.len()))
+        }
     }
 
-    fn check(&mut self, target_token: &str) {
-        assert_eq!(self.token(), target_token);
-        self.advance();
+    fn check(&mut self, target_token: &str) -> io::Result<()> {
+        let actual = self.token()?;
+        if actual == target_token {
+            self.advance();
+            Ok(())
+        } else {
+            errorize(format!("Token '{}' expected, token '{}' encountered at position {}", target_token, actual, self.i))
+        }
     }
 
     fn advance(&mut self) {
@@ -146,98 +162,108 @@ impl PddlParser {
         self.i += distance;
     }
 
-    fn define(&mut self) {
-        self.check("(");
-        self.check("define");
-        self.problem();
-        self.domain();
-        self.objects();
-        self.init();
-        self.goal();
-        self.metric();
-        self.check(")");
+    fn define(&mut self) -> io::Result<()> {
+        self.check("(")?;
+        self.check("define")?;
+        self.problem()?;
+        self.domain()?;
+        self.objects()?;
+        self.init()?;
+        self.goal()?;
+        self.metric()?;
+        self.check(")")?;
+        Ok(())
     }
 
-    fn problem(&mut self) {
-        self.check("(");
-        self.check("problem");
-        self.problem.name = String::from(self.snag());
-        self.check(")");
+    fn problem(&mut self) -> io::Result<()> {
+        self.check("(")?;
+        self.check("problem")?;
+        self.problem.name = String::from(self.snag()?);
+        self.check(")")?;
+        Ok(())
     }
 
-    fn domain(&mut self) {
-        self.check("(");
-        self.check(":domain");
-        self.problem.domain = String::from(self.snag());
-        self.check(")");
+    fn domain(&mut self) -> io::Result<()> {
+        self.check("(")?;
+        self.check(":domain")?;
+        self.problem.domain = String::from(self.snag()?);
+        self.check(")")?;
+        Ok(())
     }
 
-    fn objects(&mut self) {
-        self.check("(");
-        self.check(":objects");
-        while !self.at_close() {
-            let object_name = self.snag();
-            let object_type = if self.token() == "-" {
+    fn objects(&mut self) -> io::Result<()> {
+        self.check("(")?;
+        self.check(":objects")?;
+        while !self.at_close()? {
+            let object_name = self.snag()?;
+            let object_type = if self.token()? == "-" {
                 self.advance();
-                self.snag()
+                self.snag()?
             } else {
                 String::from("untyped")
             };
             self.problem.obj2type.insert(object_name, object_type);
         }
-        self.check(")");
+        self.check(")")?;
+        Ok(())
     }
 
-    fn init(&mut self) {
-        self.check("(");
-        self.check(":init");
-        while !self.at_close() {
-            if self.lookahead(1) == "=" {
-                self.check("(");
-                self.check("=");
-                let key = self.snag_predicate();
-                let value = self.token().parse::<f64>().unwrap();
+    fn init(&mut self) -> io::Result<()> {
+        self.check("(")?;
+        self.check(":init")?;
+        while !self.at_close()? {
+            if self.lookahead(1)? == "=" {
+                self.check("(")?;
+                self.check("=")?;
+                let key = self.snag_predicate()?;
+                let value = match self.token()?.parse::<f64>() {
+                    Ok(value) => value,
+                    Err(e) => return errorize(format!("{:?}", e))
+                };
                 self.advance();
-                self.check(")");
+                self.check(")")?;
                 self.problem.f64_state.insert(key, value);
             } else {
-                let pred = self.snag_predicate();
+                let pred = self.snag_predicate()?;
                 self.problem.bool_state.insert(pred);
             }
         }
-        self.check(")");
+        self.check(")")?;
+        Ok(())
     }
 
-    fn goal(&mut self) {
-        self.check("(");
-        self.check(":goal");
-        if self.token() == "(" && self.lookahead(1) == "and" {
+    fn goal(&mut self) -> io::Result<()> {
+        self.check("(")?;
+        self.check(":goal")?;
+        if self.token()? == "(" && self.lookahead(1)? == "and" {
             self.advance_by(2);
-            while !self.at_close() {
-                let pred = self.snag_predicate();
+            while !self.at_close()? {
+                let pred = self.snag_predicate()?;
                 self.problem.goals.insert(pred);
             }
             self.advance();
         } else {
-            let pred = self.snag_predicate();
+            let pred = self.snag_predicate()?;
             self.problem.goals.insert(pred);
         }
-        self.check(")");
+        self.check(")")?;
+        Ok(())
     }
 
-    fn metric(&mut self) {
-        if self.token() == "(" {
+    fn metric(&mut self) -> io::Result<()> {
+        if self.token()? == "(" {
             self.advance();
-            self.check(":metric");
-            let tag = self.snag();
-            let predicate = self.snag_predicate();
+            self.check(":metric")?;
+            let tag = self.snag()?;
+            let predicate = self.snag_predicate()?;
             self.problem.metric = Some(match tag.as_str() {
                 "minimize" => Metric::Minimize(predicate),
                 "maximize" => Metric::Maximize(predicate),
-                _ => panic!("Unrecognized metric tag: {}", tag)
+                _ => return errorize(format!("Unrecognized metric tag: {}", tag))
             });
-            self.check(")");
+            self.check(")")?;
         }
+        Ok(())
     }
 }
 
@@ -252,7 +278,7 @@ mod tests {
 (:objects A B)
 (:init (clear a) (clear b) (ontable a) (ontable b) (handempty))
 (:goal (on a b)))";
-        let parsed = PddlParser::parse(pddl);
+        let parsed = PddlParser::parse(pddl).unwrap();
         assert_eq!(format!("{:?}", parsed), r#"PddlProblem { name: "blocks-2-0", domain: "blocks", obj2type: {"a": "untyped", "b": "untyped"}, bool_state: {Predicate { elements: ["clear", "a"] }, Predicate { elements: ["clear", "b"] }, Predicate { elements: ["handempty"] }, Predicate { elements: ["ontable", "a"] }, Predicate { elements: ["ontable", "b"] }}, f64_state: {}, goals: {Predicate { elements: ["on", "a", "b"] }}, metric: None }"#);
     }
 
@@ -265,7 +291,7 @@ mod tests {
  (ON C B) (HANDEMPTY))
 (:goal (AND (ON A B) (ON B C) (ON C D)))
 )";
-        let parsed = PddlParser::parse(pddl);
+        let parsed = PddlParser::parse(pddl).unwrap();
         assert_eq!(format!("{:?}", parsed), r#"PddlProblem { name: "blocks-4-2", domain: "blocks", obj2type: {"a": "untyped", "b": "untyped", "c": "untyped", "d": "untyped"}, bool_state: {Predicate { elements: ["clear", "a"] }, Predicate { elements: ["clear", "c"] }, Predicate { elements: ["clear", "d"] }, Predicate { elements: ["handempty"] }, Predicate { elements: ["on", "c", "b"] }, Predicate { elements: ["ontable", "a"] }, Predicate { elements: ["ontable", "b"] }, Predicate { elements: ["ontable", "d"] }}, f64_state: {}, goals: {Predicate { elements: ["on", "a", "b"] }, Predicate { elements: ["on", "b", "c"] }, Predicate { elements: ["on", "c", "d"] }}, metric: None }"#);
     }
 
@@ -360,7 +386,7 @@ mod tests {
 (:metric minimize (fuel-used))
 
 )";
-        let parsed = PddlParser::parse(pddl);
+        let parsed = PddlParser::parse(pddl).unwrap();
         assert_eq!(format!("{:?}", parsed), r#"PddlProblem { name: "strips-sat-x-1", domain: "satellite", obj2type: {"groundstation1": "direction", "groundstation2": "direction", "image1": "mode", "instrument0": "instrument", "phenomenon3": "direction", "phenomenon4": "direction", "phenomenon6": "direction", "satellite0": "satellite", "spectrograph2": "mode", "star0": "direction", "star5": "direction", "thermograph0": "mode"}, bool_state: {Predicate { elements: ["calibration_target", "instrument0", "groundstation2"] }, Predicate { elements: ["on_board", "instrument0", "satellite0"] }, Predicate { elements: ["pointing", "satellite0", "phenomenon6"] }, Predicate { elements: ["power_avail", "satellite0"] }, Predicate { elements: ["supports", "instrument0", "thermograph0"] }}, f64_state: {Predicate { elements: ["data", "phenomenon3", "image1"] }: 22.0, Predicate { elements: ["data", "phenomenon3", "spectrograph2"] }: 125.0, Predicate { elements: ["data", "phenomenon3", "thermograph0"] }: 136.0, Predicate { elements: ["data", "phenomenon4", "image1"] }: 120.0, Predicate { elements: ["data", "phenomenon4", "spectrograph2"] }: 196.0, Predicate { elements: ["data", "phenomenon4", "thermograph0"] }: 134.0, Predicate { elements: ["data", "phenomenon6", "image1"] }: 144.0, Predicate { elements: ["data", "phenomenon6", "spectrograph2"] }: 174.0, Predicate { elements: ["data", "phenomenon6", "thermograph0"] }: 219.0, Predicate { elements: ["data", "star5", "image1"] }: 203.0, Predicate { elements: ["data", "star5", "spectrograph2"] }: 68.0, Predicate { elements: ["data", "star5", "thermograph0"] }: 273.0, Predicate { elements: ["data-stored"] }: 0.0, Predicate { elements: ["data_capacity", "satellite0"] }: 1000.0, Predicate { elements: ["fuel", "satellite0"] }: 112.0, Predicate { elements: ["fuel-used"] }: 0.0, Predicate { elements: ["slew_time", "groundstation1", "groundstation2"] }: 68.04, Predicate { elements: ["slew_time", "groundstation1", "phenomenon3"] }: 89.48, Predicate { elements: ["slew_time", "groundstation1", "phenomenon4"] }: 31.79, Predicate { elements: ["slew_time", "groundstation1", "phenomenon6"] }: 17.63, Predicate { elements: ["slew_time", "groundstation1", "star0"] }: 18.17, Predicate { elements: ["slew_time", "groundstation1", "star5"] }: 8.59, Predicate { elements: ["slew_time", "groundstation2", "groundstation1"] }: 68.04, Predicate { elements: ["slew_time", "groundstation2", "phenomenon3"] }: 33.94, Predicate { elements: ["slew_time", "groundstation2", "phenomenon4"] }: 39.73, Predicate { elements: ["slew_time", "groundstation2", "phenomenon6"] }: 50.73, Predicate { elements: ["slew_time", "groundstation2", "star0"] }: 38.61, Predicate { elements: ["slew_time", "groundstation2", "star5"] }: 62.86, Predicate { elements: ["slew_time", "phenomenon3", "groundstation1"] }: 89.48, Predicate { elements: ["slew_time", "phenomenon3", "groundstation2"] }: 33.94, Predicate { elements: ["slew_time", "phenomenon3", "phenomenon4"] }: 25.72, Predicate { elements: ["slew_time", "phenomenon3", "phenomenon6"] }: 14.75, Predicate { elements: ["slew_time", "phenomenon3", "star0"] }: 14.29, Predicate { elements: ["slew_time", "phenomenon3", "star5"] }: 10.18, Predicate { elements: ["slew_time", "phenomenon4", "groundstation1"] }: 31.79, Predicate { elements: ["slew_time", "phenomenon4", "groundstation2"] }: 39.73, Predicate { elements: ["slew_time", "phenomenon4", "phenomenon3"] }: 25.72, Predicate { elements: ["slew_time", "phenomenon4", "phenomenon6"] }: 2.098, Predicate { elements: ["slew_time", "phenomenon4", "star0"] }: 35.01, Predicate { elements: ["slew_time", "phenomenon4", "star5"] }: 64.5, Predicate { elements: ["slew_time", "phenomenon6", "groundstation1"] }: 17.63, Predicate { elements: ["slew_time", "phenomenon6", "groundstation2"] }: 50.73, Predicate { elements: ["slew_time", "phenomenon6", "phenomenon3"] }: 14.75, Predicate { elements: ["slew_time", "phenomenon6", "phenomenon4"] }: 2.098, Predicate { elements: ["slew_time", "phenomenon6", "star0"] }: 77.07, Predicate { elements: ["slew_time", "phenomenon6", "star5"] }: 29.32, Predicate { elements: ["slew_time", "star0", "groundstation1"] }: 18.17, Predicate { elements: ["slew_time", "star0", "groundstation2"] }: 38.61, Predicate { elements: ["slew_time", "star0", "phenomenon3"] }: 14.29, Predicate { elements: ["slew_time", "star0", "phenomenon4"] }: 35.01, Predicate { elements: ["slew_time", "star0", "phenomenon6"] }: 77.07, Predicate { elements: ["slew_time", "star0", "star5"] }: 36.56, Predicate { elements: ["slew_time", "star5", "groundstation1"] }: 8.59, Predicate { elements: ["slew_time", "star5", "groundstation2"] }: 62.86, Predicate { elements: ["slew_time", "star5", "phenomenon3"] }: 10.18, Predicate { elements: ["slew_time", "star5", "phenomenon4"] }: 64.5, Predicate { elements: ["slew_time", "star5", "phenomenon6"] }: 29.32, Predicate { elements: ["slew_time", "star5", "star0"] }: 36.56}, goals: {Predicate { elements: ["have_image", "phenomenon4", "thermograph0"] }, Predicate { elements: ["have_image", "phenomenon6", "thermograph0"] }, Predicate { elements: ["have_image", "star5", "thermograph0"] }}, metric: Some(Minimize(Predicate { elements: ["fuel-used"] })) }"#);
     }
 }
